2dccabd3cea8a54ef874b03fdbb7a8a307e19b28
T430S <T430S@LENOVO>
2020-08-27 18:56:35 +0530

    2nd commit

---
+++ gitEclipseProject/src/gitEclipseProject/LaunchBrowser.java
@@ -0,0 +1,22 @@
+package gitEclipseProject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+	static WebDriver driver;
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "lib//chromedriver.exe");
+		driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.youtube.com/");
+		System.out.println(driver.getTitle());
+		
+			String txt="This is a project to integrate Jenkins with Eclipse and Github";
+			System.out.println(txt);
+			System.out.println("Length of above text is "+txt.length());
+
+	}
+
+}
