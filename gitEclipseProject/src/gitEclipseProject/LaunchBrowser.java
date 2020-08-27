9cc28241e51af12eecfb2b73a97ff56c480760d8
T430S <T430S@LENOVO>
2020-08-27 20:09:34 +0530

    Commit No 5

--- gitEclipseProject/src/gitEclipseProject/LaunchBrowser.java
+++ gitEclipseProject/src/gitEclipseProject/LaunchBrowser.java
@@ -1,18 +1,22 @@
 package gitEclipseProject;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LaunchBrowser {
 	static WebDriver driver;
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "lib\\chromedriver.exe");
 		driver= new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.facebook.com/");
+		driver.get("https://www.youtube.com/");
+		Thread.sleep(1000);
 		System.out.println("Title of the browser is " +driver.getTitle());
-		
+		driver.findElement(By.xpath("//input[@placeholder='Search']")).sendKeys("Itelearn");
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//input[@placeholder='Search']")).sendKeys(Keys.ENTER);
 			String txt="This is a project to integrate Jenkins with Eclipse and Github";
 			System.out.println(txt);
 			System.out.println("Length of above text is "+txt.length());
