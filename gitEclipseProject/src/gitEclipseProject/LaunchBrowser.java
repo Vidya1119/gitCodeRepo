73667c6bb4b1ff4a5bba623b00bff2b896b07d07
T430S <T430S@LENOVO>
2020-08-27 19:07:07 +0530

    Final Commit with selenium code

--- gitEclipseProject/src/gitEclipseProject/LaunchBrowser.java
+++ gitEclipseProject/src/gitEclipseProject/LaunchBrowser.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class LaunchBrowser {
 	static WebDriver driver;
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "lib//chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "lib\\chromedriver.exe");
 		driver= new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.youtube.com/");
