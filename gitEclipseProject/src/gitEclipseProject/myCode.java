e040f04537b5ac07148b4ad28836ee18c4ee9611
T430S <T430S@LENOVO>
2020-08-27 18:10:43 +0530

    1st push

---
+++ gitEclipseProject/src/gitEclipseProject/myCode.java
@@ -0,0 +1,12 @@
+package gitEclipseProject;
+
+public class myCode {
+
+	public static void main(String[] args) {
+			String txt="This is a project to integrate Jenkins with Eclipse and Github";
+			System.out.println(txt);
+			System.out.println("Length of above text is "+txt.length());
+
+	}
+
+}
